f0159f7e044d0ca6cfb1a434a8295d9350ab65dd
Sentinel-1 <sentinel-1@users.noreply.github.com>
2022-06-16 01:00:59 +0400

    Refactor

--- Employment.py
+++ Employment.py
@@ -21,7 +21,7 @@ print(f"\nNotebook START time: {nb_st} UTC\n")
 # In[2]:
 
 
-get_ipython().run_cell_magic('HTML', '', '<script>\n  function code_toggle() {\n    if (code_shown){\n      $(\'div.input\').hide(\'500\');\n      $(\'#toggleButton\').val(\'Show Python Code\')\n    } else {\n      $(\'div.input\').show(\'500\');\n      $(\'#toggleButton\').val(\'Hide Python Code\')\n    }\n    code_shown = !code_shown\n  }\n\n  $( document ).ready(function(){\n    code_shown=false;\n    $(\'div.input\').hide();\n    $(\'div.input:contains("%%HTML")\').removeClass( "input")\n  });\n</script>\n<form action="javascript:code_toggle()"><input type="submit" id="toggleButton" value="Show Python Code"></form>\n')
+get_ipython().run_cell_magic('HTML', '', '<script>\n  function code_toggle() {\n    if (code_shown){\n      $(\'div.input\').hide(\'500\');\n      $(\'#toggleButton\').val(\'Show Python Code\')\n    } else {\n      $(\'div.input\').show(\'500\');\n      $(\'#toggleButton\').val(\'Hide Python Code\')\n    }\n    code_shown = !code_shown\n  }\n\n  $( document ).ready(function(){\n    code_shown=false;\n    $(\'div.input\').hide();\n    $(\'div.input:contains("%%HTML")\').removeClass( "input")\n  });\n</script>\n<form action="javascript:code_toggle()">\n  <input type="submit" id="toggleButton" value="Show Python Code">\n</form>\n')
 
 
 # In[3]:
@@ -29,12 +29,25 @@ get_ipython().run_cell_magic('HTML', '', '<script>\n  function code_toggle() {\n
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import geopandas
 
 
 # In[4]:
 
 
-def read_employment_data_of_year(year, target_feature, excel_path = "data/05-Labour-Force-Indicators-by-regions.xlsx"):
+VERBOSE=False
+
+
+# In[5]:
+
+
+DATA_FILE_PATH = "data/05-Labour-Force-Indicators-by-regions.xlsx"
+
+
+def read_employment_data_of_year(
+        year: int,
+        target_feature: str,
+        excel_path: str = DATA_FILE_PATH) -> pd.DataFrame:
     df = pd.read_excel(excel_path)
     df = df.loc[df.index >= df.loc[df.iloc[:,0] == year].index.item()]
     col_names = df.head(2).tail(1).values
@@ -47,7 +60,7 @@ def read_employment_data_of_year(year, target_feature, excel_path = "data/05-Lab
     return df.reset_index(drop=True)
 
 
-# In[5]:
+# In[6]:
 
 
 features_available = [
@@ -66,66 +79,66 @@ print(f"\nData for {year} year:\n")
 display(df)
 
 
-# In[6]:
+# In[7]:
 
 
 plt.figure(figsize=(18,9))
-plt.barh(data=df.loc[df['Region'] !='Georgia'], y='Region', width=target_feature)
+plt.barh(
+    data=df.loc[df['Region'] !='Georgia'],
+    y='Region',
+    width=target_feature,
+)
 plt.title(f'{year} - {target_feature} by Regions in Georgia',
           fontdict={
               'fontsize': 16,
               'fontweight': 'bold',
               'color': '#444444',
               'verticalalignment': 'baseline',
-              'horizontalalignment': 'center'
+              'horizontalalignment': 'center',
           })
 plt.xlabel('Thousand Persons')
 plt.show()
 
 
-# In[7]:
-
-
-import geopandas
-
-
 # In[8]:
 
 
-Georgia_geodf = geopandas.read_file('data/GADM/gadm40_GEO_shp/gadm40_GEO_1.shp')
+ADMINISTRATIVE_BORDERS_FILE_PATH = 'data/GADM/gadm40_GEO_shp/gadm40_GEO_1.shp'
+
+Georgia_geodf = geopandas.read_file(ADMINISTRATIVE_BORDERS_FILE_PATH)
 
 
 # In[9]:
-
-
-VERBOSE=False
-
-
-# In[10]:
 
 
 if VERBOSE:
     print(Georgia_geodf['NAME_1'].unique())
 
 
+# In[10]:
+
+
+if VERBOSE:
+    print('Check if there is any mismatch in region names '
+          'between data and map:')
+    df.set_index('Region').index.difference(Georgia_geodf['NAME_1'].unique())
+
+
 # In[11]:
 
 
 if VERBOSE:
-    print('Check if there is any mismatch in region names between data and map:')
-    df.set_index('Region').index.difference(Georgia_geodf['NAME_1'].unique())
+    print('Resolving name mismatch detected earlier '
+          'in order to connect employment data to map data...')
+
+df = df.replace(
+    {
+        'Adjara A/R': 'Ajaria',
+        'Racha-Lechkhumi and Kvemo-Svaneti': 'Racha-Lechkhumi-Kvemo Svaneti',
+    })
 
 
 # In[12]:
-
-
-if VERBOSE:
-    print('Resolving name mismatch detected earlier in order to connect employment data to map data...')
-
-df = df.replace({'Adjara A/R':'Ajaria', 'Racha-Lechkhumi and Kvemo-Svaneti':'Racha-Lechkhumi-Kvemo Svaneti'})
-
-
-# In[13]:
 
 
 if VERBOSE:
@@ -133,15 +146,21 @@ if VERBOSE:
     df.set_index('Region').index.difference(Georgia_geodf['NAME_1'].unique())
 
 
-# In[14]:
+# In[13]:
 
 
-merged_geodf = Georgia_geodf.merge(df, left_on='NAME_1', right_on='Region', how='left').fillna(0)
+merged_geodf = Georgia_geodf.merge(df,
+                                   left_on='NAME_1',
+                                   right_on='Region',
+                                   how='left').fillna(0)
+
 if VERBOSE:
-    print(f"Map data with statistics for the {len(Georgia_geodf['NAME_1'].unique())} regions of Georgia is ready.")
+    print("Map data with statistics for the "
+          f"{len(Georgia_geodf['NAME_1'].unique())} regions of Georgia "
+          "is ready.")
 
 
-# In[15]:
+# In[14]:
 
 
 merged_geodf.plot(
@@ -150,7 +169,7 @@ merged_geodf.plot(
     figsize=(18, 9),
     legend=True,
     legend_kwds = {"fmt": "{:.1f}", "title": "Thousand Persons"},
-    cmap='coolwarm'
+    cmap='coolwarm',
 )
 plt.gca().set_axis_off()
 plt.title(f'{year} - {target_feature} by Regions in Georgia',
@@ -166,7 +185,7 @@ plt.gcf().savefig("map.png")
 plt.show()
 
 
-# In[16]:
+# In[15]:
 
 
 print(f"\n ** Total Elapsed time: {datetime.utcnow() - nb_st} ** \n")
